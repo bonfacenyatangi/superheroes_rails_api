dae295caf5f1ad2fc8309d83ae0763979975988e
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-07-03 23:03:59 +0300

    Updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,17 @@ Rails.application.routes.draw do
   resources :heroes, only: [:index, :show]
   resources :powers, only: [:index, :show, :update]
   resources :hero_powers, only: [:create]
+
+    # Route for updating a power
+    patch '/powers/:id', to: 'powers#update'
+
+    # Route for handling hero not found
+    get '/heroes/:id', to: 'heroes#show', as: 'hero'
   
+    # Route for handling power not found
+    get '/powers/:id', to: 'powers#show', as: 'power'
+  
+    # Root route
+    root to: 'heroes#index'
+
 end
