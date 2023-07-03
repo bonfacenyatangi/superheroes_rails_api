4a55ca1ba37b93eb29cf1136d94b8092432d7207
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-07-03 23:16:15 +0300

    Updated the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,34 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# db/seeds.rb
-kamala_khan = Hero.create(name: 'Kamala Khan', super_name: 'Ms. Marvel')
-doreen_green = Hero.create(name: 'Doreen Green', super_name: 'Squirrel Girl')
-gwen_stacy = Hero.create(name: 'Gwen Stacy', super_name: 'Spider-Gwen')
+# Clear existing data
+HeroPower.delete_all
+Power.delete_all
+Hero.delete_all
 
-super_strength = Power.create(name: 'super strength', description: 'gives the wielder super-human strengths')
-flight = Power.create(name: 'flight', description: 'gives the wielder the ability to fly through the skies at supersonic speed')
+# Create Heroes
+heroes = [
+  { name: 'Kamala Khan', super_name: 'Ms. Marvel' },
+  { name: 'Doreen Green', super_name: 'Squirrel Girl' },
+  { name: 'Gwen Stacy', super_name: 'Spider-Gwen' }
+]
+heroes.each { |hero| Hero.create!(hero) }
 
-HeroPower.create(hero: kamala_khan, power: super_strength, strength: 'Strong')
-HeroPower.create(hero: kamala_khan, power: flight, strength: 'Average')
+# Create Powers
+powers = [
+  { name: 'Super Strength', description: 'Gives the wielder super-human strengths' },
+  { name: 'Flight', description: 'Gives the wielder the ability to fly through the skies at supersonic speed' }
+]
+powers.each { |power| Power.create!(power) }
+
+# Create HeroPowers
+hero_powers = [
+  { hero: Hero.first, power: Power.first, strength: 'Strong' },
+  { hero: Hero.first, power: Power.last, strength: 'Average' },
+  { hero: Hero.second, power: Power.first, strength: 'Weak' },
+  { hero: Hero.third, power: Power.last, strength: 'Average' }
+]
+hero_powers.each { |hero_power| HeroPower.create!(hero_power) }
+
+puts 'Seed data created successfully.'
+
