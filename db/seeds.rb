de0e328e3393e9b667ff4a060379011d76f29c30
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-07-03 22:41:06 +0300

    Generated Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+kamala_khan = Hero.create(name: 'Kamala Khan', super_name: 'Ms. Marvel')
+doreen_green = Hero.create(name: 'Doreen Green', super_name: 'Squirrel Girl')
+gwen_stacy = Hero.create(name: 'Gwen Stacy', super_name: 'Spider-Gwen')
+
+super_strength = Power.create(name: 'super strength', description: 'gives the wielder super-human strengths')
+flight = Power.create(name: 'flight', description: 'gives the wielder the ability to fly through the skies at supersonic speed')
+
+HeroPower.create(hero: kamala_khan, power: super_strength, strength: 'Strong')
+HeroPower.create(hero: kamala_khan, power: flight, strength: 'Average')
