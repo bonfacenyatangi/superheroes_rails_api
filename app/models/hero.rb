acde7782c1e8e9353e3d48df509becf230b8953a
bonface nyatangi <bonfacenyatangi@gmail.com>
2023-07-03 23:09:03 +0300

    Updated validations

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,9 @@
 class Hero < ApplicationRecord
     has_many :hero_powers
     has_many :powers, through: :hero_powers
+
+     # Validations
+  validates :name, presence: true
+  validates :super_name, presence: true
   end
   
